7328e80a810df47b44dbe80669ddc9b858bfaf3a
Keerthi Mani <carkeane.getz@gmail.com>
2020-02-10 10:20:37 -0500

    query selector for slider

--- js/materialize.js
+++ js/materialize.js
@@ -2,3 +2,11 @@
 const sideNav = document.querySelector(".sidenav");
 //Materialize js library - use M
 M.Sidenav.init(sideNav, {});
+
+//Slider
+const slider = document.querySelector(".slider");
+M.Slider.init(slider, {
+  indicators: false,
+  height: 500,
+  interval: 6000
+});
